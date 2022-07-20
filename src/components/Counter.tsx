77cbbfbac204f294addec5ebebb289654f419f90
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-20 10:57:59 -0300

    counter from React+Ts

---
+++ src/components/Counter.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [counter, setCounter] = useState(10);
+
+  const onIncrement = (number: number): void => {
+    setCounter(number + 1);
+  };
+
+  const onDecrement = (number: number): void => {
+    setCounter(number - 1);
+  };
+
+  return (
+    <div className="mt-5">
+      <h3>Counter: {counter}</h3>
+      <br />
+      <button onClick={() => onIncrement(counter)} className="btn btn-outline-primary">
+        +1
+      </button>
+      <button onClick={() => onDecrement(counter)} className="btn btn-outline-primary mx-2">
+        -1
+      </button>
+      <button onClick={() => setCounter(10)} className="btn btn-outline-danger">
+        Reset
+      </button>
+    </div>
+  );
+};
